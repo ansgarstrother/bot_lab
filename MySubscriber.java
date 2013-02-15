4c3f3d4131b000d4f580d2bdc2c7541556a6d4b5
hkuhn <hkuhn@umich.edu>
2013-02-15 11:23:39 -0500

    Odometry files written. Need to calculate the position from data still. Log file seems messed up. Real tests needed to understand data from log file.

--- MySubscriber.java
+++ MySubscriber.java
@@ -12,6 +12,8 @@ public class MySubscriber implements LCMSubscriber
     {
         this.lcm = new LCM();
         this.lcm.subscribe("10_POSE", this);
+	this.lcm.subscribe("10_MOTOR_FEEDBACK", this);
+	this.lcm.subscribe("10_PIMU", this);
     }
 
     public void messageReceived(LCM lcm, String channel, LCMDataInputStream ins)
@@ -27,6 +29,36 @@ public class MySubscriber implements LCMSubscriber
                 System.out.println("  y coord  = [ " + msg.y_pos + " ]");
 		System.out.println("  theta  = [ " + msg.theta + " ]");
             }
+	    else if (channel.equals("10_MOTOR_FEEDBACK")) {
+		motor_feedback_t msg = new motor_feedback_t(ins);
+                System.out.println("  timestamp    = " + msg.utime);
+		for (int i = 0; i < msg.nmotors; i++) {
+                	System.out.println("  encoders     = [ " + msg.encoders[i] + " ]");
+		}
+		for (int i = 0; i < msg.nmotors; i++) {
+                	System.out.println("  current     = [ " + msg.current[i] + " ]");
+		}
+		for (int i = 0; i < msg.nmotors; i++) {
+                	System.out.println("  applied voltage     = [ " + msg.applied_voltage[i] + " ]");
+		}	
+	    }
+	    else if (channel.equals("10_PIMU")) {
+		pimu_t msg = new pimu_t(ins);
+		System.out.println("  timestamp    = " + msg.utime);
+		System.out.println("  timestamp    = " + msg.utime_pimu);
+		for (int i = 0; i < 8; i++) {
+			System.out.println("  integrator     = [ " + msg.integrator[i] + " ]");
+	    	}
+		for (int i = 0; i < 3; i++) {
+			System.out.println("  accel     = [ " + msg.accel[i] + " ]");
+	    	}
+		for (int i = 0; i < 3; i++) {
+			System.out.println("  mag     = [ " + msg.mag[i] + " ]");
+	    	}
+		for (int i = 0; i < 2; i++) {
+			System.out.println("  alttemp     = [ " + msg.alttemp[i] + " ]");
+	    	}
+	    }
 
         } catch (IOException ex) {
             System.out.println("Exception: " + ex);
