67f5f49a6bc73039dadad69142e774a6ccede80c
Hobey Kuhn <hkuhn@umich.edu>
2013-02-16 17:23:05 -0500

    Fixed a bunch. No subscribing, we're actually just sending messages and retrieving sensor information from orc board class

--- Rover/RoverOdometry.java
+++ Rover/RoverOdometry.java
@@ -5,32 +5,27 @@ import lcm.lcm.*;
 import java.lang.Math;
 import lcmtypes.*;
 
-public class RoverOdometry implements LCMSubscriber {
-
-    // LCM
-    LCM lcm;
-    float odom_timestamp;
-    float odom_timestamp_pimu;
-
+public class RoverOdometry {
+    
     // motor feedback
     int nmotors;
     int odom_encoders[];
     double  odom_current[];
     double  odom_applied_voltage[];
-
+    
     // pimu feedback
     int odom_integrator[];
     int odom_accel[];
     int odom_mag[];
     int odom_alttemp[];
-
+    
     // pos_t vars
     float odom_x;
     float odom_y;
     double odom_theta;  // IN RADIANS
     
     // covar_t vars
-    // matrix form: 
+    // matrix form:
     //  [   var_x   a       ]
     //  [   a       var_y   ]
     //  vector form: [ var_x, a, var_y ]
@@ -43,69 +38,24 @@ public class RoverOdometry implements LCMSubscriber {
     public RoverOdometry() throws IOException {
         // retrieve position from sensors/gyro
         // assign to class variables
-	this.lcm = new LCM();
-	this.lcm.subscribe("10_MOTOR_FEEDBACK", this);
-	this.lcm.subscribe("10_PIMU", this);
-
-	odom_encoders = new int[nmotors];
-	odom_current = new double[nmotors];
-	odom_applied_voltage = new double[nmotors];
-
-	odom_integrator = new int[8];
-	odom_accel = new int[3];
-	odom_mag = new int[3];
-	odom_alttemp = new int[2];
+        nmotors = 2;
+        
+        odom_encoders = new int[nmotors];
+        odom_current = new double[nmotors];
+        odom_applied_voltage = new double[nmotors];
+        
+        odom_integrator = new int[8];
+        odom_accel = new int[3];
+        odom_mag = new int[3];
+        odom_alttemp = new int[2];
         
     }
     
-    // LCM CLASS METHODS
-    public void messageReceived(LCM lcm, String channel, LCMDataInputStream ins) {
-
-	//System.out.println("Received message on channel " + channel);
-
-        try {
-	    if (channel.equals("10_MOTOR_FEEDBACK")) {
-		motor_feedback_t msg = new motor_feedback_t(ins);
-                odom_timestamp = msg.utime;
-		for (int i = 0; i < msg.nmotors; i++) {
-                	odom_encoders[i] = msg.encoders[i];
-		}
-		for (int i = 0; i < msg.nmotors; i++) {
-                	odom_current[i] = msg.current[i];
-		}
-		for (int i = 0; i < msg.nmotors; i++) {
-                	odom_applied_voltage[i] = msg.applied_voltage[i];
-		}	
-	    }
-	    else if (channel.equals("10_PIMU")) {
-		pimu_t msg = new pimu_t(ins);
-		//System.out.println("  timestamp    = " + msg.utime); WHICH TIMESTAMP TO USE?
-		odom_timestamp_pimu = msg.utime_pimu;
-		for (int i = 0; i < 8; i++) {
-			odom_integrator[i] = msg.integrator[i];
-	    	}
-		for (int i = 0; i < 3; i++) {
-			odom_accel[i] = msg.accel[i];
-	    	}
-		for (int i = 0; i < 3; i++) {
-			odom_mag[i] = msg.mag[i];
-	    	}
-		for (int i = 0; i < 2; i++) {
-			odom_alttemp[i] = msg.alttemp[i];
-	    	}
-	    }
-
-        } catch (IOException ex) {
-            System.err.println("Exception: " + ex);
-        }
-	
-    }
-
     // PRIVATE CLASS METHODS
     private void calculatePose() {
-	
-
-	
+        
+        // TALK TO ORC BOARD HERE
+        
     }
     
     // PUBLIC ACTION METHODS
@@ -132,12 +82,46 @@ public class RoverOdometry implements LCMSubscriber {
         lcmtypes.covar_t covar_msg = new lcmtypes.covar_t();
         covar_msg.timestamp = concurrent_timestamp;
         covar_msg.var_x = odom_var_x;
-	covar_msg.var_y = odom_var_y;
-	covar_msg.a = odom_var_a;
+        covar_msg.var_y = odom_var_y;
+        covar_msg.a = odom_var_a;
+        
+        // instantiate a new lcm pimu message
+        lcmtypes.pimu_t pimu_msg = new lcmtypes.pimu_t();
+        pimu_msg.utime = concurrent_timestamp;
+        pimu_msg.utime_pimu = System.nanoTime();
+        for (int i = 0; i < odom_integrator.length; i++) {
+            pimu_msg.integrator[i] = odom_integrator[i];
+        }
+        for (int i = 0; i < odom_accel.length; i++) {
+            pimu_msg.accel[i] = odom_accel[i];
+        }
+        for (int i = 0; i < odom_mag.length; i++) {
+            pimu_msg.mag[i] = odom_mag[i];
+        }
+        for (int i = 0; i < odom_alttemp.length; i++) {
+            pimu_msg.alttemp[i] = odom_alttemp[i];
+        }
+        
+        // instantiate a new lcm motor feedback message
+        lcmtypes.motor_feedback_t mf_msg = new lcmtypes.motor_feedback_t();
+        mf_msg.utime = concurrent_timestamp;
+        mf_msg.nmotors = nmotors;
+        for (int i = 0; i < odom_encoders.length; i++) {
+            mf_msg.encoders[i] = odom_encoders[i];
+        }
+        for (int i = 0; i < odom_current.length; i++) {
+            mf_msg.current[i] = odom_current[i];
+        }
+        for (int i = 0; i < odom_applied_voltage.length; i++) {
+            mf_msg.applied_voltage[i] = odom_applied_voltage[i];
+        }
+            
         
         //publish
         lcm.publish("10_POSE", pos_msg);
         lcm.publish("10_COVAR", covar_msg);
-
+        lcm.publish("10_PIMU", pimu_msg);
+        lcm.publish("10_MOTOR_FEEDBACK", mf_msg);
+        
     }
 }
