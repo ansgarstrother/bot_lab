3338026c71d1970ce146f3bdbef0097e0c1800f7
hkuhn <hkuhn@umich.edu>
2013-02-21 22:12:34 -0500

    Finished Blue Line Detection

--- Vision/util/binarize.java
+++ Vision/util/binarize.java
@@ -19,15 +19,24 @@ public class binarize {
 
 	protected void binarizeImage() {
 		for (int i = 0; i < in.getWidth(); i++) {
-			for (int j = (in.getHeight()/2 - 100); j < in.getHeight(); j++) {
-				int r = in.getRGB(i,j) & 0xff;
+			for (int j = 0; j < (in.getHeight()/2 + 100); j++) {
+				int x = i;
+				int y = in.getHeight() - 1 - j;
+				int r = in.getRGB(x,y) & 0xff;
 				if (r > thresh) {
 					//int newPixel = 16777215;	// 255 255 255
 					//edited.setRGB(i,j, newPixel);
 				}
 				else {
 					int newPixel = 0;
-					binarized.setRGB(i,j,newPixel);
+					binarized.setRGB(x,y,newPixel);
+					binarized.setRGB(x,y-1,newPixel);	
+					binarized.setRGB(x,y-2,newPixel);
+					binarized.setRGB(x,y-3,newPixel);
+					binarized.setRGB(x,y-4,newPixel);
+					binarized.setRGB(x,y-5,newPixel);	
+					binarized.setRGB(x,y-6,newPixel);
+					break;			
 				}
 				
 			}
--- Vision/util/edgeDetection.java
+++ Vision/util/edgeDetection.java
@@ -8,11 +8,46 @@ public class edgeDetection {
 	BufferedImage in;
 	int height;
 	int width;
+	int[] boundaryMap;
 
 	public edgeDetection(BufferedImage im) {
 		in = im;
 		height = im.getHeight();
 		width = im.getWidth();
+		boundaryMap = new int[width];	// index = x coord, value = y coord
+		detectEdges();
+	}
+
+	protected void detectEdges() {
+		// Scan image going up, left to right
+		// Search for solid black pixels (5 after first encounter
+		// Set pixel to white and move to next column
+		// White pixels denote boundary
+		for (int i = 0; i < width; i++) {
+			int x = i;
+			for (int j = 0; j < (height/2) + 100; j++) {
+				int y = height - 1 - j;
+				int rgb = in.getRGB(x,y);
+				int r = (rgb >> 16) & 0xff;
+				int g = (rgb >> 8) & 0xff;
+				int b = rgb & 0xff;
+				if ((r + g + b) == 0) {		// black pixel
+					in.setRGB(x,y,0xffffffff);
+					in.setRGB(x,y-1,0xffffffff);	// add thickness
+					boundaryMap[x] = y;
+					break;
+				}
+			}
+		}
+					
+				
+	}
+
+	public BufferedImage getImage() {
+		return in;
+	}
+	public int[] getBoundaryMap() {
+		return boundaryMap;
 	}
 
 }
