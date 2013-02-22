b2e784d1ad73994c2fc4a354b91a855ea5ecb349
astrothe <astrothe@umich.edu>
2013-02-22 18:41:10 -0500

    Triangle dectection is now fast

--- Rover/PositionCalulator.java
+++ Rover/PositionCalulator.java
@@ -2,7 +2,8 @@ package Rover;
 import java.io.*;
 import lcm.lcm.*;
 import java.lang.Math;
-import lcmtypes.*;
+import april.jmat.Matrix.*;
+import java.util.Vector;
 
 public class PositionCalulator {
 
@@ -14,15 +15,31 @@ public class PositionCalulator {
 		float errorX;
     	float locY;
 		float errorY;
-		float errorTheta; 
+		float errorTheta;
    		double theta;  // IN RADIANS
 	}
 
-	static Vector<Location> prevLocations = new Vector<Location>;
+	static Vector<Move> moves = new Vector<Move>();
 
-	public PostisionCalulator(int OdCalibration, int roverBase){
+    static class Move{
+		float distance;
+   		double theta;  // IN RADIANS
+    }
+
+
+    static Location curLocation = new Location();
+
+
+	public void PostisionCalulator(int OdCalibration, int roverBase){
 		OdCal = OdCalibration;
 		base = roverBase;
+        curLocation.locX = 0;
+        curLocation.locY = 0;
+        curLocation.theta = 0;
+        curLocation.errorX = 0;
+        curLocation.errorY = 0;
+        curLocation.errorTheta = 0;
+
 	}
 
 
@@ -33,15 +50,43 @@ public class PositionCalulator {
 	static double turned ( int disL, int disR){
 		return (disR-disL)/base;
 	}
-		
-	public Location updatelocation(int ticksL, int ticksR){
-		
+
+    static void updatePosition(Move move){
+
+        curLocation.theta += move.theta;
+        curLocation.locX += move.distance * Math.cos(curLocation.theta);
+        curLocation.locY += move.distance * Math.sin(curLocation.theta);
+
+        curLocation.errorTheta += .01 * move.theta;
+        curLocation.errorX += .01 * move.distance * Math.cos(curLocation.theta);
+        curLocation.errorY += .01 * move.distance * Math.sin(curLocation.theta);
+    }
+
+	public void updatelocation(int ticksL, int ticksR){
 		int disL = ticksL * OdCal;
 		int disR = ticksR * OdCal;
-		
-		
+
+
+        Move curMove = new Move();
+
+        //calculate the movement
+        curMove.distance = moved(disL, disR);
+        curMove.theta = turned(disL, disR);
+
+        //Update the postition
+        updatePosition(curMove);
+
+        moves.add(curMove);
 	}
 
+    public double[] curPostion(){
 
+        double temp[] = {curLocation.locX, curLocation.locY, curLocation.theta};
+        return temp;
+    }
 
+    public float[] curError(){
+        float temp[] = {curLocation.errorX, curLocation.errorY, curLocation.errorTheta};
+        return temp;
+    }
 }
--- Vision/TargetDetection/TargetDetectionDetector.java
+++ Vision/TargetDetection/TargetDetectionDetector.java
@@ -14,7 +14,7 @@ public class TargetDetectionDetector {
 	int delimiter;		// restricts height
 	double threshold;	// RGB threshold
 
-	
+    int group = 1;
 
 	// CONSTRUCTOR METHOD
 	public TargetDetectionDetector(BufferedImage in, int delimiter, double thresh) {
@@ -24,10 +24,68 @@ public class TargetDetectionDetector {
 		height = in.getHeight();
 		this.delimiter = delimiter;
 		threshold = thresh;
+        group = 1;
 		runDetection();
 	}
 
-	
+    void expand(int[][] green, int i, int j){
+        im.setRGB(i,j,0xff00ff00);
+        green[i][j] = group;
+
+        if(green[i+1][j] == 0){
+	        int cRGB = im.getRGB(i+1,j);
+
+            // convert to HSV
+		    float[] cHSV = RGBtoHSV(cRGB >> 16 & 0xff, cRGB >> 8 & 0xff, cRGB & 0xff);
+		    //System.out.println(cHSV[0]);
+		    if (cHSV[0] > 0.15 && cHSV[0] < 0.4 && cHSV[1] > 0.4 && cHSV[2] > 0.3) {
+                expand(green, i+1, j);
+            }
+        }
+
+        if(green[i-1][j] == 0){
+
+            int cRGB = im.getRGB(i-1,j);
+
+            // convert to HSV
+		    float[] cHSV = RGBtoHSV(cRGB >> 16 & 0xff, cRGB >> 8 & 0xff, cRGB & 0xff);
+		    //System.out.println(cHSV[0]);
+	    	if (cHSV[0] > 0.15 && cHSV[0] < 0.4 && cHSV[1] > 0.4 && cHSV[2] > 0.3) {
+                expand(green, i-1, j);
+            }
+        }
+
+
+        if(green[i][j+1] == 0){
+
+            int cRGB = im.getRGB(i,j+1);
+
+            // convert to HSV
+		    float[] cHSV = RGBtoHSV(cRGB >> 16 & 0xff, cRGB >> 8 & 0xff, cRGB & 0xff);
+	    	//System.out.println(cHSV[0]);
+		    if (cHSV[0] > 0.15 && cHSV[0] < 0.4 && cHSV[1] > 0.4 && cHSV[2] > 0.3) {
+                expand(green, i, j+1);
+            }
+        }
+
+
+        if(green[i][j-1] == 0){
+
+            int cRGB = im.getRGB(i,j-1);
+
+             // convert to HSV
+	    	float[] cHSV = RGBtoHSV(cRGB >> 16 & 0xff, cRGB >> 8 & 0xff, cRGB & 0xff);
+	    	//System.out.println(cHSV[0]);
+		    if (cHSV[0] > 0.15 && cHSV[0] < 0.4 && cHSV[1] > 0.4 && cHSV[2] > 0.3) {
+                 expand(green, i, j-1);
+            }
+        }
+
+
+
+    }
+
+
 	// PROTECTED METHODS
 	protected void runDetection() {
 		// Union Find searching for pixels of high green intensity
@@ -43,32 +101,31 @@ public class TargetDetectionDetector {
 		//	a. For each pixel, find root node and update stats in hash map
 		// For all root nodes in hash map
 		//	a. Build a covariance matrix and use stats to determine shape of pixel blob
-		for (int j = (height/2); j < height - delimiter; j++) {
-			for (int i = 0; i < width - 1; i++) {
+
+        int green[][] = new int[width][height];
+		for (int j = (height/2); j < height - delimiter; j += 10) {
+			for (int i = (j%10); i < width - 1; i += 40) {
 				int cRGB = im.getRGB(i,j);
-				int rRGB = im.getRGB(i+1,j);
-				int dRGB = im.getRGB(i,j+1);
 				// convert to HSV
 				float[] cHSV = RGBtoHSV(cRGB >> 16 & 0xff, cRGB >> 8 & 0xff, cRGB & 0xff);
-				float[] rHSV = RGBtoHSV(rRGB >> 16 & 0xff, rRGB >> 8 & 0xff, rRGB & 0xff);
-				float[] dHSV = RGBtoHSV(dRGB >> 16 & 0xff, dRGB >> 8 & 0xff, dRGB & 0xff);
 				//System.out.println(cHSV[0]);
 				if (cHSV[0] > 0.15 && cHSV[0] < 0.4 && cHSV[1] > 0.4 && cHSV[2] > 0.3) {
-					im.setRGB(i,j,0xff00ff00);
-				}
+                    expand(green, i, j);
+                    group++;
+                }
 
-				
+
 
 				boolean current_is_green = false;
-				
+
 			}
 		}
-				
 
 
 
 
-	
+
+
 	}
 
 	// HELPER FUNCTIONS
@@ -77,15 +134,15 @@ public class TargetDetectionDetector {
 		Color.RGBtoHSB(r,g,b,HSV);
 		return HSV;
 	}
-	
+
 
 	// PUBLIC METHODS
 	public BufferedImage getImage() {
 		return im;
 	}
 
-	
-	
+
+
 
 
 
