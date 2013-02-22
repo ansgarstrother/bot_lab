5b8f451b1ecfc65f5214a488dc4099de333ec7c5
astrothe <astrothe@umich.edu>
2013-02-22 18:42:16 -0500

    Triangle dectection is now fast

--- Vision/TargetDetection/TargetDetectionDetector.java
+++ Vision/TargetDetection/TargetDetectionDetector.java
@@ -3,10 +3,13 @@ package Vision.TargetDetection;
 
 import java.awt.image.BufferedImage;
 import java.awt.Color;
+import java.util.Vector;
 
 public class TargetDetectionDetector {
 
 	// args
+    static final int BLOB_SIZE_CONSTANT = 100;
+
 	BufferedImage im;
 	BufferedImage out;	// bounding box w/center at triangle
 	int width;
@@ -15,6 +18,16 @@ public class TargetDetectionDetector {
 	double threshold;	// RGB threshold
 
     int group = 1;
+
+    class Stats{
+        int count;
+        int maxX;
+        int maxY;
+        int minX;
+        int minY;
+    }
+
+    Vector<Stats> stats = new Vector<Stats>();
 
 	// CONSTRUCTOR METHOD
 	public TargetDetectionDetector(BufferedImage in, int delimiter, double thresh) {
@@ -31,6 +44,9 @@ public class TargetDetectionDetector {
     void expand(int[][] green, int i, int j){
         im.setRGB(i,j,0xff00ff00);
         green[i][j] = group;
+        Stats s = stats.get(group-1);
+        s.count++;
+        stats.set(group-1, s);
 
         if(green[i+1][j] == 0){
 	        int cRGB = im.getRGB(i+1,j);
@@ -39,6 +55,12 @@ public class TargetDetectionDetector {
 		    float[] cHSV = RGBtoHSV(cRGB >> 16 & 0xff, cRGB >> 8 & 0xff, cRGB & 0xff);
 		    //System.out.println(cHSV[0]);
 		    if (cHSV[0] > 0.15 && cHSV[0] < 0.4 && cHSV[1] > 0.4 && cHSV[2] > 0.3) {
+
+                s = stats.get(group-1);
+                if(i+1 > s.maxY){
+                    s.maxY = i+1;
+                    stats.set(group-1, s);
+                }
                 expand(green, i+1, j);
             }
         }
@@ -51,6 +73,12 @@ public class TargetDetectionDetector {
 		    float[] cHSV = RGBtoHSV(cRGB >> 16 & 0xff, cRGB >> 8 & 0xff, cRGB & 0xff);
 		    //System.out.println(cHSV[0]);
 	    	if (cHSV[0] > 0.15 && cHSV[0] < 0.4 && cHSV[1] > 0.4 && cHSV[2] > 0.3) {
+                
+                s = stats.get(group-1);
+                if(i+1 < s.minY){
+                    s.minY = i-1;
+                    stats.set(group-1, s);
+                }
                 expand(green, i-1, j);
             }
         }
@@ -64,6 +92,12 @@ public class TargetDetectionDetector {
 		    float[] cHSV = RGBtoHSV(cRGB >> 16 & 0xff, cRGB >> 8 & 0xff, cRGB & 0xff);
 	    	//System.out.println(cHSV[0]);
 		    if (cHSV[0] > 0.15 && cHSV[0] < 0.4 && cHSV[1] > 0.4 && cHSV[2] > 0.3) {
+
+                s = stats.get(group-1);
+                if(j+1 > s.maxX){
+                    s.maxX = j+1;
+                    stats.set(group-1, s);
+                }
                 expand(green, i, j+1);
             }
         }
@@ -77,7 +111,14 @@ public class TargetDetectionDetector {
 	    	float[] cHSV = RGBtoHSV(cRGB >> 16 & 0xff, cRGB >> 8 & 0xff, cRGB & 0xff);
 	    	//System.out.println(cHSV[0]);
 		    if (cHSV[0] > 0.15 && cHSV[0] < 0.4 && cHSV[1] > 0.4 && cHSV[2] > 0.3) {
-                 expand(green, i, j-1);
+
+                s = stats.get(group-1);
+                
+                if(j-1 < s.minX){
+                    s.minX = j-1;
+                    stats.set(group-1, s);
+                }
+                expand(green, i, j-1);
             }
         }
 
@@ -110,23 +151,51 @@ public class TargetDetectionDetector {
 				float[] cHSV = RGBtoHSV(cRGB >> 16 & 0xff, cRGB >> 8 & 0xff, cRGB & 0xff);
 				//System.out.println(cHSV[0]);
 				if (cHSV[0] > 0.15 && cHSV[0] < 0.4 && cHSV[1] > 0.4 && cHSV[2] > 0.3) {
+                    Stats temp = new Stats();
+                    temp.minX = 1000000;
+                    temp.minY = 1000000;
+                    stats.add(temp);
                     expand(green, i, j);
                     group++;
                 }
-
-
-
-				boolean current_is_green = false;
-
 			}
 		}
 
-
+        for (Stats s : stats){
+            if (s.count > BLOB_SIZE_CONSTANT){
+                int [] bounds = {s.minY, s.minX, s.maxY, s.maxX};
+                mark(im, bounds, 0xffff0000);
+                System.out.print("MinY: ");
+                System.out.println(s.minY);
+                System.out.print("MinY: ");
+                System.out.println(s.minY);
+                System.out.print("MinX: ");
+                System.out.println(s.minX);
+                System.out.print("MaxY: ");
+                System.out.println(s.maxY);
+                System.out.print("MaxX: ");
+                System.out.println(s.maxX);
+            }
+        }
 
 
 
 
 	}
+	
+	public void mark(BufferedImage img, int[] bounds, int color){
+        // draw the horizontal lines
+        for (int x = bounds[0]; x <=bounds[2]; x++) {
+            img.setRGB(x,bounds[1], color); //Go Blue!
+            img.setRGB(x,bounds[3], color); //Go Blue!
+        }
+
+        // draw the horizontal lines
+        for (int y = bounds[1]; y <=bounds[3]; y++) {
+            img.setRGB(bounds[0],y, color); //Go Blue!
+            img.setRGB(bounds[2],y, color); //Go Blue
+        }
+     }
 
 	// HELPER FUNCTIONS
 	protected float[] RGBtoHSV(int r, int g, int b) {
