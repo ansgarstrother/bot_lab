ca45841e2459f7bf80b1f98d7bd93ba81e402d6f
Ansgar <ansgar@ansgar-U46E>
2013-02-27 11:39:33 -0500

    Set up to run on Panda Bot

--- Panda/PandaTargetDetector.java
+++ Panda/PandaTargetDetector.java
@@ -1,28 +1,28 @@
 package Panda;
 
-
 import java.awt.image.BufferedImage;
 import java.awt.Color;
 import java.util.Vector;
 
-public class TargetDetectionDetector {
+public class PandaTargetDetector {
 
 	//Pixel area of triangle must be greater then this size to be counted
     static final int BLOB_SIZE_CONSTANT = 150;
     //Triangle must be within 100 pixels of half of its bounding box
     static final int TRI_HALF_BOX_THRES_CONSTANT = 100;
-    	// HSV LIMITS
-	static double HLB;		// hue lower bound
-	static double HUB;		// hue upper bound
-	static double SLB;		// saturation lower bound
-	static double VLB;
+    
+	// HSV LIMITS
+
+	//TODO need to actually find out what these values are
+	static double HLB = 0;		// hue lower bound
+	static double HUB = 50;		// hue upper bound
+	static double SLB = 0;		// saturation lower bound
+	static double VLB = 50;
 
 	BufferedImage im;
 	BufferedImage out;	// bounding box w/center at triangle
-	TargetDetectionController tdc;
 	int width;
 	int height;
-	int delimiter;		// restricts height
 
     int group = 1;
 
@@ -40,18 +40,11 @@ public class TargetDetectionDetector {
     Vector<Stats> finalTriVec = new Vector<Stats>();
 
 	// CONSTRUCTOR METHOD
-	public TargetDetectionDetector(BufferedImage in, int delimiter, TargetDetectionController tdc) {
+	public PandaTargetDetector(BufferedImage in) {
 		im = in;
-		this.tdc = tdc;
 		width = in.getWidth();
 		height = in.getHeight();
-		this.delimiter = delimiter;
-		double[] thresholds = tdc.getThresholds();
-		HLB = thresholds[0];
-		HUB = thresholds[1];
-		SLB = thresholds[2];
-		VLB = thresholds[3];
-        	group = 1;
+       	group = 1;
 		runDetection();
 	}
 
@@ -67,7 +60,7 @@ public class TargetDetectionDetector {
 
             // convert to HSV
 		    float[] cHSV = RGBtoHSV(cRGB >> 16 & 0xff, cRGB >> 8 & 0xff, cRGB & 0xff);
-		    //System.out.println(cHSV[0]);
+
 		    if (cHSV[0] > HLB && cHSV[0] < HUB && cHSV[1] > SLB && cHSV[2] > VLB) {
 
                 s = stats.get(group-1);
@@ -85,10 +78,10 @@ public class TargetDetectionDetector {
 
             // convert to HSV
 		    float[] cHSV = RGBtoHSV(cRGB >> 16 & 0xff, cRGB >> 8 & 0xff, cRGB & 0xff);
-		    //System.out.println(cHSV[0]);
-	    	if (cHSV[0] > HLB && cHSV[0] < HUB && cHSV[1] > SLB && cHSV[2] > VLB) {
-                
-                s = stats.get(group-1);
+	
+	    	if (cHSV[0] > HLB && cHSV[0] < HUB && cHSV[1] > SLB && cHSV[2] > VLB) {                
+              
+				s = stats.get(group-1);
                 if(i+1 < s.minY){
                     s.minY = i-1;
                     stats.set(group-1, s);
@@ -123,7 +116,7 @@ public class TargetDetectionDetector {
 
              // convert to HSV
 	    	float[] cHSV = RGBtoHSV(cRGB >> 16 & 0xff, cRGB >> 8 & 0xff, cRGB & 0xff);
-	    	//System.out.println(cHSV[0]);
+
 		    if (cHSV[0] > HLB && cHSV[0] < HUB && cHSV[1] > SLB && cHSV[2] > VLB) {
 
                 s = stats.get(group-1);
@@ -143,27 +136,17 @@ public class TargetDetectionDetector {
 
 	// PROTECTED METHODS
 	protected void runDetection() {
-		// Union Find searching for pixels of high green intensity
-		// Traverse Entire Image
-		//	a. Retrieve current pixel, right pixel, and down pixel
-		//		- current pixel must pass threshold test for the following to occur:
-		//	b. If right pixel passes threshold test
-		//		1. if parent to right pixel == self, then update to current pixel
-		//		2. else if parent to right pixel != cur_pixel, trace parent back and link roots to cur_pixel
-		//	c. Repeat with down pixel
-		// Build Hash Table of stats useful to detect a triangle
-		// Traverse Entire Image
-		//	a. For each pixel, find root node and update stats in hash map
-		// For all root nodes in hash map
-		//	a. Build a covariance matrix and use stats to determine shape of pixel blob
 
         int green[][] = new int[width][height];
-		for (int j = (height/2); j < height - delimiter; j += 10) {
+
+		for (int j = (height/2); j < height; j += 10) {
 			for (int i = (j%10); i < width - 1; i += 40) {
+
 				int cRGB = im.getRGB(i,j);
+
 				// convert to HSV
 				float[] cHSV = RGBtoHSV(cRGB >> 16 & 0xff, cRGB >> 8 & 0xff, cRGB & 0xff);
-				//System.out.println(cHSV[0]);
+
 				if (cHSV[0] > HLB && cHSV[0] < HUB && cHSV[1] > SLB && cHSV[2] > VLB) {
                     Stats temp = new Stats();
                     temp.minX = 1000000;
@@ -178,7 +161,6 @@ public class TargetDetectionDetector {
         for (Stats s : stats){
             if (s.count > BLOB_SIZE_CONSTANT){
                 int [] bounds = {s.minY, s.minX, s.maxY, s.maxX};
-                mark(im, bounds, 0xffff0000);
                 s.centerX = (s.maxX - s.minX) / 2;
                 s.centerY = (s.maxY - s.minY) / 2;
 
@@ -197,21 +179,6 @@ public class TargetDetectionDetector {
 
 	}
 	
-	protected void mark(BufferedImage img, int[] bounds, int color){
-        // draw the horizontal lines
-        for (int x = bounds[0]; x <=bounds[2]; x++) {
-            img.setRGB(x,bounds[1], color); //Go Blue!
-            img.setRGB(x,bounds[3], color); //Go Blue!
-        }
-
-        // draw the horizontal lines
-        for (int y = bounds[1]; y <=bounds[3]; y++) {
-            img.setRGB(bounds[0],y, color); //Go Blue!
-            img.setRGB(bounds[2],y, color); //Go Blue
-        }
-     }
-
-	// HELPER FUNCTIONS
 	protected float[] RGBtoHSV(int r, int g, int b) {
 		float[] HSV = new float[3];
 		Color.RGBtoHSB(r,g,b,HSV);
