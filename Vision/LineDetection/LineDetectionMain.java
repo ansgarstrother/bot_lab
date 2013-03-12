feb2da65b9f20e4c1151e2f1c6ae4036213abbf0
hkuhn <hkuhn@umich.edu>
2013-03-12 18:34:04 -0400

    wrote a new script for calibration conversion using ray projection. check it out in PandaPositioning.java

--- Vision/LineDetection/LineDetectionMain.java
+++ Vision/LineDetection/LineDetectionMain.java
@@ -11,8 +11,8 @@ public class LineDetectionMain {
 	private final static double f = 422.51110623757427;
 	private final static double c_x = 654.9986200773297;
 	private final static double c_y = 949.9890978166349;
-    private static double[][] calibrationMatrix =
-		{ 	{f, 0, c_x}, {0, f, c_y}, {0, 0, 1}	};
+    private static double[] calibrationMatrix =
+		{ 	f, c_x, c_y	};
 
 
     public static void main(String[] args) {
@@ -22,8 +22,8 @@ public class LineDetectionMain {
                 // create app frame
                 LineDetectionFrame frame = new LineDetectionFrame();
                 // build controller
-				ArrayList<Matrix> history = new ArrayList<Matrix>();
-                LineDetectionController appController = new LineDetectionController(frame, calibrationMatrix, history);
+				PandaPositioning pp = new PandaPositioning();
+                LineDetectionController appController = new LineDetectionController(frame, calibrationMatrix, pp);
 
             }
         });
