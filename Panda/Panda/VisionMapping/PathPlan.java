f13740648a66796d64ffa1b4ae47117c103b6c28
Ansgar <ansgar@ansgar-U46E>
2013-03-16 15:03:14 -0400

    Added PathPlan skeleton

---
+++ Panda/Panda/VisionMapping/PathPlan.java
@@ -0,0 +1,61 @@
+package Panda.VisionMapping;
+
+import java.util.ArrayList;
+import javax.swing.*;
+import java.awt.*;
+import java.math.*;
+
+public class PathPlan{
+
+	protected int map[][];
+
+//=================================================================//
+// PathPlan                                                        //
+//                                                                 //
+// Initailizes A* path plan class.                                 //
+//                                                                 //
+// Returns: VOID                                                   //
+//=================================================================//
+	public void PathPlan(){
+
+	}
+
+//=================================================================//
+// plan                                                            //
+//                                                                 //
+// Runs A* algorithm and and sets pathAngle and pathDistance       //
+//                                                                 //
+// Returns: VOID                                                   //
+//=================================================================//
+	public void plan(){
+
+	}
+
+//=================================================================//
+//getPathAngle                                                     //
+//                                                                 //
+// Returns path calculated by plan                                 //
+//                                                                 //
+// Returns: double                                                 //
+//=================================================================//
+	public double getPathAngle(){
+
+	}
+	
+//=================================================================//
+//getPathDistance                                                  //
+//                                                                 //
+// Returns path distance calculated in plan                        //
+//                                                                 //
+// Returns: double                                                 //
+//=================================================================//
+	public double getPathDistance(){
+
+	}
+    
+}
+
+
+
+
+		
